128e6310091dc0c560ebc2d80ea03e792311d644
Simon Oulevay <simon.oulevay@lotaris.com>
2014-03-24 10:10:53 +0100

    Completed client spec.

--- spec/client.spec.js
+++ spec/client.spec.js
@@ -40,6 +40,20 @@ describe("Client", function() {
       };
     });
 
+    function makeRequest(req, res, definition) {
+
+      var done = false;
+
+      runs(function() {
+        requestMock[res instanceof Error ? 'addError' : 'addResponse'](res);
+        client.request(req).fin(function() { done = true; });
+      });
+
+      waitsFor(function() {
+        return done;
+      }, 'The client request should be completed', 50);
+    }
+
     it("should not accept options that are not an object", function() {
       _.each([ null, 'string', [], 4.2 ], function(invalidOptions) {
         expect(function() {
@@ -169,38 +183,96 @@ describe("Client", function() {
         client.configure({ logger: logger });
       });
 
-      it("should log HTTP requests with the DEBUG level", function() {
+      it("should log successful HTTP requests with the DEBUG level", function() {
 
-        var done = false;
-        runs(function() {
-          requestMock.addResponse(simpleResponse);
-          client.request(minimalRequestOptions).fin(function() { done = true; });
-        });
-
-        waitsFor(function() {
-          return done;
-        }, "The client request should be completed", 50);
+        makeRequest(minimalRequestOptions, simpleResponse);
 
         runs(function() {
           expect(logger.debug.calls.length).toBe(2);
           expect(logger.debug.calls[0].args).toEqual([ "http[1]".cyan + ' GET /foo' ]);
           expect(logger.debug.calls[1].args[0]).toMatch(new RegExp(RegExp.escape("http[1]".cyan + ' ' + '204 No Content'.green + ' in ') + '\\d+ms$'));
         });
+      });
 
-        done = false;
+      it("should not log failed HTTP requests with the DEBUG level", function() {
+
+        makeRequest(minimalRequestOptions, new Error('foo'));
 
         runs(function() {
-          requestMock.addError(new Error('foo'));
-          client.request(minimalRequestOptions).fin(function() { done = true; });
+          expect(logger.debug.calls.length).toBe(1);
+          expect(logger.debug.calls[0].args).toEqual([ "http[1]".cyan + ' GET /foo' ]);
+        });
+      });
+
+      it("should log the HTTP status code in yellow if not in the 200-399 range", function() {
+
+        makeRequest(minimalRequestOptions, { statusCode: 400, body: 'epic fail' });
+
+        runs(function() {
+          expect(logger.debug.calls.length).toBe(2);
+          expect(logger.debug.calls[0].args).toEqual([ "http[1]".cyan + ' GET /foo' ]);
+          expect(logger.debug.calls[1].args[0]).toMatch(new RegExp(RegExp.escape("http[1]".cyan + ' ' + '400 Bad Request'.yellow + ' in ') + '\\d+ms$'));
+        });
+      });
+
+      it("should number HTTP requests in the order they are made", function() {
+
+        _.each([ simpleResponse, { statusCode: 500, body: 'oops' }, { statusCode: 200, body: 'bar' } ], function(response) {
+          makeRequest(minimalRequestOptions, response);
         });
 
-        waitsFor(function() {
-          return done;
-        }, "The client request should be completed", 50);
+        runs(function() {
+          expect(logger.debug.calls.length).toBe(6);
+          expect(logger.debug.calls[0].args).toEqual([ "http[1]".cyan + ' GET /foo' ]);
+          expect(logger.debug.calls[1].args[0]).toMatch(new RegExp(RegExp.escape("http[1]".cyan + ' ' + '204 No Content'.green + ' in ') + '\\d+ms$'));
+          expect(logger.debug.calls[2].args).toEqual([ "http[2]".cyan + ' GET /foo' ]);
+          expect(logger.debug.calls[3].args[0]).toMatch(new RegExp(RegExp.escape("http[2]".cyan + ' ' + '500 Internal Server Error'.yellow + ' in ') + '\\d+ms$'));
+          expect(logger.debug.calls[4].args).toEqual([ "http[3]".cyan + ' GET /foo' ]);
+          expect(logger.debug.calls[5].args[0]).toMatch(new RegExp(RegExp.escape("http[3]".cyan + ' ' + '200 OK'.green + ' in ') + '\\d+ms$'));
+        });
+      });
+
+      it("should log HTTP request options with the `showRequest` option", function() {
+
+        client.configure({ showRequest: true });
+        makeRequest(minimalRequestOptions, simpleResponse);
 
         runs(function() {
           expect(logger.debug.calls.length).toBe(3);
-          expect(logger.debug.calls[2].args).toEqual([ "http[2]".cyan + ' GET /foo' ]);
+          expect(logger.debug.calls[0].args).toEqual([ "http[1]".cyan + ' GET /foo' ]);
+          expect(logger.debug.calls[1].args).toEqual([ 'http[1]'.cyan + ' request options: ' + JSON.stringify({ url: '/foo', method: 'GET' }).magenta ]);
+          expect(logger.debug.calls[2].args[0]).toMatch(new RegExp(RegExp.escape("http[1]".cyan + ' ' + '204 No Content'.green + ' in ') + '\\d+ms$'));
+        });
+      });
+
+      describe("with the `showResponseBody` option", function() {
+
+        beforeEach(function() {
+          client.configure({ showResponseBody: true });
+        });
+
+        it("should not log the HTTP response body if there is none", function() {
+
+          makeRequest(minimalRequestOptions, simpleResponse);
+
+          runs(function() {
+            expect(logger.debug.calls.length).toBe(2);
+            expect(logger.debug.calls[0].args).toEqual([ "http[1]".cyan + ' GET /foo' ]);
+            expect(logger.debug.calls[1].args[0]).toMatch(new RegExp(RegExp.escape("http[1]".cyan + ' ' + '204 No Content'.green + ' in ') + '\\d+ms$'));
+          });
+        });
+
+        it("should log the HTTP response body", function() {
+
+          var response = { statusCode: 200, body: { bar: 'baz' } };
+          makeRequest(minimalRequestOptions, response);
+
+          runs(function() {
+            expect(logger.debug.calls.length).toBe(3);
+            expect(logger.debug.calls[0].args).toEqual([ "http[1]".cyan + ' GET /foo' ]);
+            expect(logger.debug.calls[1].args[0]).toMatch(new RegExp(RegExp.escape("http[1]".cyan + ' ' + '200 OK'.green + ' in ') + '\\d+ms$'));
+            expect(logger.debug.calls[2].args).toEqual([ 'http[1]'.cyan + ' response body: ' + JSON.stringify(response.body).magenta ]);
+          });
         });
       });
     });
