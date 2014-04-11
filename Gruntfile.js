d35143aad5b43d67622b76924bc7f94586bc9135
Simon Oulevay <simon.oulevay@gmail.com>
2014-04-11 18:17:07 +0200

    Generate and publish doc with grunt-docker and grunt-gh-pages.

--- Gruntfile.js
+++ Gruntfile.js
@@ -18,12 +18,48 @@ module.exports = function(grunt) {
         specNameMatcher: 'spec'
       },
       all: [ 'spec/' ]
+    },
+
+    docker: {
+      options: {
+        inDir: 'lib',
+        colourScheme: 'native'
+      },
+      doc: {
+        src: [ 'lib' ],
+        dest: 'doc/annotated'
+      },
+      ghp: {
+        src: [ 'lib' ],
+        dest: 'tmp/gh-pages/annotated'
+      }
+    },
+
+    'gh-pages': {
+      options: {
+        base: 'tmp/gh-pages',
+        add: true,
+        message: 'Updated annotated source'
+      },
+      all: {
+        src: '**'
+      }
+    },
+
+    clean: {
+      doc: [ 'doc' ],
+      ghp: [ 'tmp/gh-pages' ]
     }
   });
 
   grunt.loadNpmTasks('grunt-bump');
+  grunt.loadNpmTasks('grunt-docker');
   grunt.loadNpmTasks('grunt-jasmine-node');
   grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-gh-pages');
+  grunt.loadNpmTasks('grunt-contrib-clean');
 
   grunt.registerTask('default', [ 'jshint', 'jasmine_node' ]);
+  grunt.registerTask('doc', [ 'clean:doc', 'docker:doc' ]);
+  grunt.registerTask('publish-doc', [ 'clean:ghp', 'docker:ghp', 'gh-pages' ]);
 };
